3743278934f02cc136c31ff88288f4126f523ec8
pjsinco <psinco@fas.harvard.edu>
2015-03-07 08:45:34 -0600

    get slide nav, pager to appear

--- elit-responsive-slides-shortcode.php
+++ elit-responsive-slides-shortcode.php
@@ -11,6 +11,21 @@ function elit_responsive_slides_shortcode( $atts ) {
   $a = shortcode_atts( 
     array(
       'ids' => '',
+      'auto' => true,
+      'timeout' => 4000,
+      'pager' => true,
+      'nav' => true,
+      'random' => false,
+      'pause' => true,
+      'pauseControls' => true,
+      'prevText' => 'Previous',
+      'nextText' => 'Next',
+      'maxwidth' => '728',
+      //'navContainer' => '',
+      //'manualControls' => '',
+      'namespace' => 'rslides',
+      //'before' => '',
+      //'after' => '',
     ),
     $atts
   );
@@ -20,38 +35,42 @@ function elit_responsive_slides_shortcode( $atts ) {
   $output  = '<div class="elit-slideshow">';
   $output .= '<ul class="elit-slideshow__list" id="elit-slideshow">';
   
-  //foreach ( $ids as $id ) {
-    $output .= '<li class="elit-slideshow__item">';
-    $output .= '<img src="http://elit.dev/wp-content/uploads/2015/03/do-day-mckenna.jpg" />';
+  foreach ( $ids as $id ) {
+    $attachment = get_post( $id );
+    $image_url = wp_get_attachment_image_src( $id, 'elit-large', false ); 
+    $output .= '<li class="elit-slideshow__list-item">';
+    $output .= '<img class="image__img elit-slideshow__img" src="' . $image_url[0] .  '" />';
     $output .= '</li>';
 
-    $output .= '<li class="elit-slideshow__item">';
-    $output .= '<img src="http://elit.dev/wp-content/uploads/2015/03/do-day-hashtag.jpg" />';
-    $output .= '</li>';
-
-    $output .= '<li class="elit-slideshow__item">';
-    $output .= '<img src="http://elit.dev/wp-content/uploads/2015/03/do-day-7.jpg" />';
-    $output .= '</li>';
-
-  //}
+  }
   $output .= "</ul>";
   $output .= "</div>";
 
-  echo $output;
 
   wp_enqueue_script( 'responsive-slides-js' );
-  add_action( 'wp_footer', 'elit_call_responsive_slides_js', 50);
-  
+  //add_action( 'wp_footer', 'elit_call_responsive_slides_js', 50);
+  add_action( 
+    'wp_footer', 
+    function() use ( $a ) {
+      $script  = '<script>';
+      $script .= 'jQuery(function() {';
+      $script .= 'jQuery(\'#elit-slideshow\').responsiveSlides({';
+      $script .= 'auto: ' . $a['auto'] . ',';
+      //$script .= 'auto: true,';
+      $script .= 'timeout: ' . $a['timeout'] . ',';
+      //$script .= 'timeout: 4000,';
+      $script .= 'pager: ' . $a['pager'] . ',';
+      //$script .= 'pager: true,';
+      $script .= 'nav: ' . $a['nav'] . ',';
+      //$script .= 'maxwidth: ' . $a['maxwidth'] . ',';
+      $script .= 'maxwidth: "728"';
+      $script .= '});';
+      $script .= '});';
+      $script .= '</script>';
+      echo $script;
+    } 
+  );
+  return $output;
 }
-
 add_shortcode( 'elit-slideshow' , 'elit_responsive_slides_shortcode' );
 
-function elit_call_responsive_slides_js() {
-  $output  = '<script>';
-  $output .= 'jQuery(function() {';
-  $output .= 'jQuery(\'#elit-slideshow\').responsiveSlides();';
-  $output .= '});';
-  $output .= '</script>';
-
-  echo $output;
-}
